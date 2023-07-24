d3ffcb5fbf352c42c24bf780409a6087950c2b87
USER <USER@SUSHMA>
2023-07-24 15:06:49 +0530

    Second commit

---
+++ TestProject/src/Calculator.java
@@ -0,0 +1,14 @@
+
+public class Calculator {
+	int n1=5,n2=2;
+	
+	public void add()
+	{
+		System.out.println("sum is"+(n1+n2));
+	}
+public static void main(String []args) {
+		Calculator c=new Calculator();
+		c.add();
+	}
+
+}
